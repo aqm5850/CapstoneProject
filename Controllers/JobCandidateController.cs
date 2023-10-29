59ea64a79e8fb59c67bca49e6139e14e90f180c5
aqm5850 <147955859+aqm5850@users.noreply.github.com>
2023-10-28 22:52:47 -0400

    Add files via upload

--- Controllers/JobCandidateController.cs
+++ Controllers/JobCandidateController.cs
@@ -1,12 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutomatedRecruiter.Context;
+using AutomatedRecruiter.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AutomatedRecruiter.Controllers
 {
     public class JobCandidateController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDBContext _dbContext;
+
+        public JobCandidateController(ApplicationDBContext dbContext)
         {
-            return View();
+            _dbContext = dbContext;
+        }
+        public IActionResult Index(JobCandidate candidate)
+        {
+            string name = candidate.CandidateName;
+            string email = candidate.CandidateEmail;
+            string phone = candidate.CandidatePhoneNumber;
+            string address = candidate.CandidateStreetAddress;
+            string education = candidate.CandidateEducation;
+            string experience = candidate.CandidateExperience;
+            string city = candidate.CandidateCity;
+            string state = candidate.CandidateState;
+            using (var context = _dbContext)
+            {
+                context.JobCandidates.AddRange(new JobCandidate
+                {
+                    CandidateEmail = email,
+                    CandidatePhoneNumber = phone,
+                    CandidateEducation = education,
+                    CandidateSkills = experience,
+                    CandidateName = name,
+                    CandidateCity= city,
+                    CandidateState= state,
+                    CandidateExperience= experience,
+                    CandidateStreetAddress = address
+
+                });
+                context.SaveChanges();  
+            }
+
+                return View();
         }
     }
 }
