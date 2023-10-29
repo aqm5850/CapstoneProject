e424310289d6c22e7fd2bb5a0a54fc3f4fa51fdc
aqm5850 <147955859+aqm5850@users.noreply.github.com>
2023-10-28 22:42:43 -0400

    Add files via upload

---
+++ JobCandidate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutomatedRecruiter.Models
+{
+    public class JobCandidate
+    {
+        [Key]
+        public int Id { get; set; }
+        public int JobId { get; set; }
+        public string CandidateName { get; set; }
+        public string CandidateSkills { get; set; }
+        public string CandidateEducation { get; set; }
+        public string CandidateExperience { get; set; }
+        public string CandidateState { get; set; }
+        public string CandidateCity { get; set; }
+        public string CandidateStreetAddress { get; set; }
+
+        public string CandidateEmail { get; set; }
+        public string CandidatePhoneNumber { get; set; }
+       
+    }
+
+}
