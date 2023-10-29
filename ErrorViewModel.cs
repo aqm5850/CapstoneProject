75692e37f0426372dc56eda02128d1d7e141b19f
aqm5850 <147955859+aqm5850@users.noreply.github.com>
2023-10-28 22:47:59 -0400

    Delete ErrorViewModel.cs

--- ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AutomatedRecruiter.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
